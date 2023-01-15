23abd50fd7debc718b60addf9be21887657a7a1b
Bojacnik <74513524+Bojacnik@users.noreply.github.com>
2023-01-15 16:52:33 +0100

    fix: included abstraction in getit

--- lib/src/getit.dart
+++ lib/src/getit.dart
@@ -1,7 +1,8 @@
 import 'package:ahoj/src/connection/connection_checker.dart';
+import 'package:ahoj/src/connection/connection_checker_abs.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 void setup() {
-  getIt.registerSingleton<ConnectionCheckerImpl>(ConnectionCheckerImpl());
+  getIt.registerSingleton<ConnectionChecker>(ConnectionCheckerImpl());
 }
