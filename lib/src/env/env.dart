c151053d6793f6acea6cd9be23495d55216cd425
Bojacnik <vitek2004@seznam.cz>
2023-01-19 12:59:41 +0100

    dart: implemented env

---
+++ lib/src/env/env.dart
@@ -0,0 +1,12 @@
+import 'package:envied/envied.dart';
+
+part 'env.g.dart';
+
+@Envied()
+abstract class Env {
+  @EnviedField(varName: 'KEY', obfuscate: true)
+  static final key = _Env.key;
+
+  @EnviedField(varName: 'STRING1')
+  static final STRING1 = _Env.STRING1;
+}
